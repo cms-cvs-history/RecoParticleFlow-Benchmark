aaab9365cd4aff9efeb22e397bb15453e12ea6d7
Joanna Weng <sha1-e1d5fb97f74876fe25d59750eed1d5611df817d0@cern.ch>
2008-04-11 16:54:14 +0000

    Added Axis Labels

--- interface/PFBenchmarkAna.h
+++ interface/PFBenchmarkAna.h
@@ -8,12 +8,12 @@
 
 #include <string>
 
-#include "TFile.h"
-#include "TH1F.h"
-#include "TH2F.h"
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TFile.h>
 
-class DaqMonitorBEInterface;
-class PFBenchmarkAlgo;
+//class DQMStore; // CMSSW_2_X_X
+class DaqMonitorBEInterface; // CMSSW_1_X_X
 
 class PFBenchmarkAna {
 public:
@@ -21,10 +21,10 @@ public:
   PFBenchmarkAna();
   virtual ~PFBenchmarkAna();
 
-  void setup(DaqMonitorBEInterface *DQM = NULL);
-  // template <typename RecoCandidate, typename GenCandidate, template <typename> class edm::view, template <typename> class VectorU>
-
-  void fill(const edm::View<reco::Candidate> *, const edm::View<reco::Candidate> *, bool PlotAgainstReco = true);
+  //void setup(DQMStore *DQM = NULL); // CMSSW_2_X_X
+  void setup(DaqMonitorBEInterface *DQM = NULL); // CMSSW_1_X_X
+  template <typename RecoCandidate, typename GenCandidate, template <typename> class VectorT, template <typename> class VectorU>
+  void fill(const VectorT<RecoCandidate> *, const VectorU<GenCandidate> *, bool PlotAgainstReco = true);
   void write(std::string Filename);
 
 private:
@@ -43,29 +43,87 @@ private:
 
   TH1F *hDeltaEta;
   TH2F *hDeltaEtavsEt;
-  TH2F *hDeltaEtaOverEtavsEt;
+  TH2F *hDeltaEtaOverEtavsEt; // ms: propose remove
   TH2F *hDeltaEtavsEta;
-  TH2F *hDeltaEtaOverEtavsEta;
-  TH2F *hDeltaEtavsPhi;
-  TH2F *hDeltaEtaOverEtavsPhi;
+  TH2F *hDeltaEtaOverEtavsEta; // ms: propose remove
+  TH2F *hDeltaEtavsPhi; // ms: propose remove
+  TH2F *hDeltaEtaOverEtavsPhi; // ms: propose remove
 
   TH1F *hDeltaPhi;
   TH2F *hDeltaPhivsEt;
-  TH2F *hDeltaPhiOverPhivsEt;
+  TH2F *hDeltaPhiOverPhivsEt; // ms: propose remove
   TH2F *hDeltaPhivsEta;
-  TH2F *hDeltaPhiOverPhivsEta;
-  TH2F *hDeltaPhivsPhi;
-  TH2F *hDeltaPhiOverPhivsPhi;
+  TH2F *hDeltaPhiOverPhivsEta; // ms: propose remove
+  TH2F *hDeltaPhivsPhi; // ms: propose remove
+  TH2F *hDeltaPhiOverPhivsPhi; // ms: propose remove
 
   TH1F *hDeltaR;
   TH2F *hDeltaRvsEt;
   TH2F *hDeltaRvsEta;
-  TH2F *hDeltaRvsPhi;
+  TH2F *hDeltaRvsPhi; // ms: propose remove
 
 protected:
 
-  DaqMonitorBEInterface *dbe_;
+  //DQMStore *dbe_;
   PFBenchmarkAlgo *algo_;
 
 };
+
+template <typename RecoCandidate, typename GenCandidate, template <typename> class VectorT, template <typename> class VectorU>
+void PFBenchmarkAna::fill(const VectorT<RecoCandidate> *RecoCollection, const VectorU<GenCandidate> *GenCollection, bool PlotAgainstReco) {
+
+  // loop over reco particles
+  for (unsigned int i = 0; i < RecoCollection->size(); i++) {
+
+    // generate histograms comparing the reco and truth candidate (truth = closest in delta-R)
+    const RecoCandidate *particle = &(*RecoCollection)[i];
+    const GenCandidate *gen_particle = algo_->matchByDeltaR(particle,GenCollection);
+
+    // get the quantities to place on the denominator and/or divide by
+    double et, eta, phi;
+    if (PlotAgainstReco) { et = particle->et(); eta = particle->eta(); phi = particle->phi(); }
+    else { et = gen_particle->et(); eta = gen_particle->eta(); phi = gen_particle->phi(); }
+    
+    // get the delta quantities
+    double deltaEt = algo_->deltaEt(particle,gen_particle);
+    double deltaR = algo_->deltaR(particle,gen_particle);
+    double deltaEta = algo_->deltaEta(particle,gen_particle);
+    double deltaPhi = algo_->deltaPhi(particle,gen_particle);
+    
+    // fill histograms
+    hDeltaEt->Fill(deltaEt);
+    hDeltaEtvsEt->Fill(et,deltaEt);
+    hDeltaEtOverEtvsEt->Fill(et,deltaEt/et);
+    hDeltaEtvsEta->Fill(eta,deltaEt);
+    hDeltaEtOverEtvsEta->Fill(eta,deltaEt/et);
+    hDeltaEtvsPhi->Fill(phi,deltaEt);
+    hDeltaEtOverEtvsPhi->Fill(phi,deltaEt/et);
+    hDeltaEtvsDeltaR->Fill(deltaR,deltaEt);
+    hDeltaEtOverEtvsDeltaR->Fill(deltaR,deltaEt/et);
+    
+    hDeltaEta->Fill(deltaEta);
+    hDeltaEtavsEt->Fill(et,deltaEta/eta);
+    hDeltaEtaOverEtavsEt->Fill(et,deltaEta/eta);
+    hDeltaEtavsEta->Fill(eta,deltaEta);
+    hDeltaEtaOverEtavsEta->Fill(eta,deltaEta/eta);
+    hDeltaEtavsPhi->Fill(phi,deltaEta);
+    hDeltaEtaOverEtavsPhi->Fill(phi,deltaEta/eta);
+    
+    hDeltaPhi->Fill(deltaPhi);
+    hDeltaPhivsEt->Fill(et,deltaPhi);
+    hDeltaPhiOverPhivsEt->Fill(et,deltaPhi/phi);
+    hDeltaPhivsEta->Fill(eta,deltaPhi);
+    hDeltaPhiOverPhivsEta->Fill(eta,deltaPhi/phi);
+    hDeltaPhivsPhi->Fill(phi,deltaPhi);
+    hDeltaPhiOverPhivsPhi->Fill(phi,deltaPhi/phi);
+
+    hDeltaR->Fill(deltaR);
+    hDeltaRvsEt->Fill(et,deltaR);
+    hDeltaRvsEta->Fill(eta,deltaR);
+
+  }
+
+}
+
+
 #endif // RecoParticleFlow_Benchmark_PFBenchmarkAna_h
